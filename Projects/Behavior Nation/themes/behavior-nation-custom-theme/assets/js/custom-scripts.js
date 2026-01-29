bbf194bc8855cecaad6770e970d150cac13ebfb0
Diksha Sharma <36410355+dishu1722@users.noreply.github.com>
2026-01-29 18:33:53 -0500

    Add success stories tabs and video popup functionality

--- Projects/Behavior Nation/themes/behavior-nation-custom-theme/assets/js/custom-scripts.js
+++ Projects/Behavior Nation/themes/behavior-nation-custom-theme/assets/js/custom-scripts.js
@@ -1,1 +1,72 @@
+/* ============================================================
+   1. SUCCESS STORIES TABS
+   ============================================================ */
+jQuery(document).ready(function($) {
+    // Tab switching logic for Success Stories
+    $('.tabs2 ul li').on('click', function() {
+        // 1. Get the index of the clicked tab
+        var tabIndex = $(this).index();
+
+        // 2. Manage tab active states
+        $('.tabs2 ul li').removeClass('current');
+        $(this).addClass('current');
+
+        // 3. Manage content box visibility
+        $('.box-content .box').hide().removeClass('current'); // Hide all
+        $('.box-content .box').eq(tabIndex).fadeIn().addClass('current'); // Show clicked index
+    });
+
+/* ============================================================
+   2. VIDEO POPUP CAROUSEL (OWL CAROUSEL)
+   ============================================================ */
+  
+   // [Your Owl Carousel Code Here]
+  // Initialize Video Slider
+    $('.care-edu-video-slider').owlCarousel({
+        loop: true,
+        margin: 21,
+        nav: true,
+        dots: true,
+        navText: [
+            "<img src='.../arrow-white.png'>", 
+            "<img src='.../arrow-white.png'>"
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+    // Reconstructed logic for Video Popups (using a typical Lightbox)
+    $('.videobox-block a').on('click', function(e) {
+        e.preventDefault();
+        var videoUrl = $(this).attr('href');
+        // Logic to trigger a modal/lightbox would go here
+        console.log("Opening video popup for: " + videoUrl);
+    });
+
+  
+  /* ============================================================
+   3. Resource Filter Logic
+   ============================================================ */
+  
+  $('#my-filter li').on('click', function() {
+        var filterIndex = $(this).index();
+
+        // Update active tab styling
+        $('#my-filter li').removeClass('current');
+        $(this).addClass('current');
+
+        // Toggle visibility of the resource blocks
+        $('.filteration-blogs .box').hide();
+        $('.filteration-blogs .box').eq(filterIndex).stop().fadeIn(500);
+    });
+});
 
